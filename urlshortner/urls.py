789d74c1b7b4846e5d67401d562cf8a7da4fd8f4
shekela <lukashekelashvili@gmail.com>
2022-07-21 14:14:26 +0400

    first commit

--- urlshortner/urls.py
+++ urlshortner/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('shortner.urls'))
+    path('',include('shortner.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
